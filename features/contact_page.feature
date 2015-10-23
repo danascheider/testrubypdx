670c547bdd7fc56dfe0c379062852cdfe7632781
Dana Scheider <dana.scheider@gmail.com>
2015-10-22 18:10:26 -0700

    Tests for the contact page and form

--- features/contact_page.feature
+++ features/contact_page.feature
@@ -8,3 +8,8 @@ Feature: Contact Page
     When I visit the contact page
     Then I should see the text 'CONTACT US'
     And I should see the contact form
+
+  Scenario: Submit the contact form
+    Given I am on the contact page
+    When I submit the contact form
+    Then I should see the text 'Thank you! Your message has been sent.'
--- features/step_definitions/contact_page_steps.rb
+++ features/step_definitions/contact_page_steps.rb
@@ -1,5 +1,17 @@
+Given(/^I am on the contact page$/) do 
+  step 'I visit the contact page'
+end
+
 When(/^I visit the contact page$/) do 
   visit '/contact'
+end
+
+When(/^I submit the contact form$/) do 
+  fill_in 'name', with: 'John Doe'
+  fill_in 'email', with: 'johndoe@example.com'
+  fill_in 'subject', with: 'Talk proposal'
+  fill_in 'body', with: 'I want to talk about Minitest'
+  click_button 'SUBMIT'
 end
 
 Then(/^I should see the contact form$/) do 
