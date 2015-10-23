5b1549a1317e9823921a0447e6a0f23105e34639
Dana Scheider <dana.scheider@gmail.com>
2015-10-22 18:18:54 -0700

    Add meeting resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :meetings
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -7,8 +8,9 @@ Rails.application.routes.draw do
   root 'site#index'
 
   get 'contact' => 'site#contact'
+  post 'contact' => 'site#send_mail'
 
-  resources :users
+  get 'about' => 'site#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
