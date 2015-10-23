22c7e7c998eca460015af914414578635d3cbf1d
Dana Scheider <dana.scheider@gmail.com>
2015-10-23 10:37:16 -0700

    Passing feature for past meetings when there are past meetings

--- features/past_meetings.feature
+++ features/past_meetings.feature
@@ -1,8 +1,14 @@
 Feature: Past meetings page
 
-  Scenario: Visit past meetings page
+  Scenario: No past meetings
     Given there are no past meetings
     And there is 1 upcoming meeting
     When I visit the 'Past Meetings' page
     Then I should see the text "We haven't had any meetings yet!"
     And I should see a link to the 'Upcoming Meetings' page
+
+  Scenario: Two past meetings
+    Given there are 2 past meetings
+    And there is 1 upcoming meeting
+    When I visit the 'Past Meetings' page
+    Then I should see 2 meetings listed
--- features/step_definitions/past_meetings_steps.rb
+++ features/step_definitions/past_meetings_steps.rb
@@ -2,6 +2,10 @@ Given(/^there are no past meetings$/) do
   Meeting.past.count == 0
 end
 
+Given(/^there are (\d+) past meetings$/) do |count|
+  @past_meetings = FactoryGirl.create_list(:past_meeting, count.to_i)
+end
+
 Given(/^there (?:is|are) (\d+) upcoming meeting(?:s?)$/) do |count|
   @upcoming_meetings = FactoryGirl.create_list(:upcoming_meeting, count.to_i)
 end
@@ -12,4 +16,10 @@ end
 
 Then(/^I should see a link to the 'Upcoming Meetings' page$/) do 
   expect(page).to have_css "a[href='/meetings/upcoming']"
+end
+
+Then(/^I should see (\d+) meetings listed$/) do|count|
+  # The meetings are displayed in a table with one <tr> per meeting, but
+  # there is also a <tr> for the header row, hence the + 1
+  expect(page).to have_css "tr", count: count.to_i + 1
 end
