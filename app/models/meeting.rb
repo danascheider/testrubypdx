8d93517ebc7166482acbb1ad9035687edf28f42b
Dana Scheider <dana.scheider@gmail.com>
2015-10-23 10:07:43 -0700

    Validate presence of date and create past and upcoming scopes

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,6 @@
 class Meeting < ActiveRecord::Base
+  validates_presence_of :date
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
