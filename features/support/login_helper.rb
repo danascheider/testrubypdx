f760ade7dcbe52388d1924b5c0e7767bebf46129
Dana Scheider <dana.scheider@gmail.com>
2015-10-25 23:11:19 -0700

    Create user through GUI instead of using FactoryGirl because that way wasn't working

--- features/support/login_helper.rb
+++ features/support/login_helper.rb
@@ -4,4 +4,23 @@ module LoginHelper
       click_link 'LOGOUT'
     end
   end
+
+  def create_user_through_gui
+    visit '/users/new'
+    fill_in 'user_name', with: 'foobarbaz'
+    fill_in 'user_password', with: 'foobarbaz'
+    fill_in 'user_password_confirmation', with: 'foobarbaz'
+    fill_in 'user_email', with: 'foobar@baz.com'
+    click_button 'Create User'
+
+    User.last
+  end
+
+  def ensure_logged_in
+    @user = create_user_through_gui
+    visit '/login'
+    fill_in 'username', with: @user.name
+    fill_in 'password', with: 'foobarbaz'
+    click_button 'Login'
+  end
 end
