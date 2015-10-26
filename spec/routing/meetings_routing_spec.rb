b29dce41a273cc8aae08c3ccfe586e44fafc9f32
Dana Scheider <dana.scheider@gmail.com>
2015-10-25 18:44:15 -0700

    Delete index route

--- spec/routing/meetings_routing_spec.rb
+++ spec/routing/meetings_routing_spec.rb
@@ -3,10 +3,6 @@ require "rails_helper"
 RSpec.describe MeetingsController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/meetings").to route_to("meetings#index")
-    end
-
     it "routes to #new" do
       expect(:get => "/meetings/new").to route_to("meetings#new")
     end
