6a94e7f3f1d098fa05278ae268abfa998480c84d
Dana Scheider <dana.scheider@gmail.com>
2015-10-26 10:10:32 -0700

    Validate presence of :name attribute on user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do 
   factory :user do
+    sequence :name do |n|
+      "user#{n}"
+    end
+    
     sequence :email do |n| 
       "user-#{n}@example.com"
     end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe User, type: :model do
   it { is_expected.to respond_to(:first_name) }
   it { is_expected.to respond_to(:last_name) }
   it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password_digest) }
+  it { is_expected.to respond_to(:name) }
+
+  let(:valid_attributes) do 
+    {
+      name: 'foobarbaz3',
+      password: 'foobarbaz',
+      email: 'foobar@baz.com'
+    }
+  end
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  it 'is invalid without a name' do 
+    user.name = nil
+    expect(user).not_to be_valid
+  end
 end
