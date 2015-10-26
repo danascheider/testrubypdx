8361320f07fb3a3acb76be8d395c8a8b5a4ddc7b
Dana Scheider <dana.scheider@gmail.com>
2015-10-25 19:31:04 -0700

    Define step for page not having certain text

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -14,6 +14,10 @@ Then(/^I should see the text "(.*)"$/) do |text|
   expect(page).to have_content text
 end
 
+Then(/^I should not see the text "([^"]*)"$/) do |text|
+  expect(page).not_to have_content text
+end
+
 Then(/^I should see the top navigation bar$/) do 
   expect(page).to have_css '#top-nav'
 end
