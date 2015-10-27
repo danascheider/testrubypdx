90a2a28ae010d333d90cd28dc1306e6b81df0245
Dana Scheider <dana.scheider@gmail.com>
2015-10-27 14:13:09 -0700

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151023185502) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "meetings", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at",  null: false
@@ -31,6 +34,6 @@ ActiveRecord::Schema.define(version: 20151023185502) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
