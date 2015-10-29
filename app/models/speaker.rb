b98e4274e04b9fd7c8fd9bd572755ae1d0d85736
Dana Scheider <dana.scheider@gmail.com>
2015-10-29 16:23:35 -0700

    Basic model specs and add validation to first name

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,4 +1,6 @@
 class Speaker < ActiveRecord::Base
   has_many :talks
   has_many :meetings, through: :talks
+
+  validates_presence_of :first_name
 end
--- spec/models/speaker_spec.rb
+++ spec/models/speaker_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Speaker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to :first_name }
+  it { is_expected.to respond_to :last_name }
+  it { is_expected.to respond_to :company }
+  it { is_expected.to respond_to :website }
+
+  describe "validations" do 
+    let(:speaker) { Speaker.new(first_name: 'Bob', last_name: 'Smith') }
+
+    it "is invalid without a first name" do 
+      speaker.first_name = nil
+      expect(speaker).not_to be_valid
+    end
+  end
 end
