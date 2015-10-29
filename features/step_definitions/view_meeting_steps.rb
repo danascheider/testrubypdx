9eb7b8ed2f96037f9a0f171bcdbc8d6987a1d9db
Dana Scheider <dana.scheider@gmail.com>
2015-10-29 16:07:34 -0700

    Test that the meeting's talks are displayed on its page

--- features/step_definitions/view_meeting_steps.rb
+++ features/step_definitions/view_meeting_steps.rb
@@ -2,6 +2,12 @@ Given /^there (?:is|are) (\d+) meetings?$/ do |count|
   @meeting = FactoryGirl.create(:meeting, {description: 'The quick brown fox jumped over the lazy dog'});
 end
 
+Given /^the meeting has (\d+) talks$/ do |count|
+  count.to_i.times do 
+    FactoryGirl.create(:talk, meeting_id: @meeting.id)
+  end
+end
+
 When /^I visit the meeting's view page$/ do 
   visit "/meetings/#{@meeting.id}"
 end
--- features/view_meeting.feature
+++ features/view_meeting.feature
@@ -2,6 +2,8 @@ Feature: View meeting
 
   Scenario: View meetings
     Given there is 1 meeting
+    And the meeting has 2 talks
     When I visit the meeting's view page
     Then I should see the meeting's display_date
     And I should see the meeting's description
+    And I should see information about the talks
