84ec8e864f9f4e524e23e1b39c5e4d25bbeb6198
Dana Scheider <dana.scheider@gmail.com>
2015-10-29 16:35:22 -0700

    Validate the presence of a title for each talk

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,4 +1,6 @@
 class Talk < ActiveRecord::Base
   belongs_to :speaker
   belongs_to :meeting
+
+  validates_presence_of :title
 end
--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Talk, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to(:title) }
+  it { is_expected.to respond_to(:description) }
+
+  describe "validations" do 
+    let(:talk) { Talk.new(title: 'All About RSpec') }
+
+    it "is invalid without a title" do 
+      talk.title = nil
+      expect(talk).not_to be_valid
+    end
+  end
 end
