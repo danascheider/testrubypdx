71242453ef105cd8a9be84c43f4755faaff0e52d
Dana Scheider <dana.scheider@gmail.com>
2015-10-29 16:24:40 -0700

    Add valid and invalid attributes hashes to the tests

--- spec/controllers/speakers_controller_spec.rb
+++ spec/controllers/speakers_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe SpeakersController, type: :controller do
   # Speaker. As you add validations to Speaker, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    {first_name: 'Craig', last_name: 'Smith'}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {first_name: nil}
   }
 
   # This should return the minimal set of values that should be in the session
