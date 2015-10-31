8e2d5335e99bde68271ab4a6874a2204f559b6c0
Dana Scheider <dana.scheider@gmail.com>
2015-10-31 13:42:36 -0700

    Show edit and destroy links only if an admin is logged in

--- spec/views/speakers/show.html.erb_spec.rb
+++ spec/views/speakers/show.html.erb_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "speakers/show", type: :view do
+  let(:user) { FactoryGirl.create(:user, id: 1) }
+
   before(:each) do
     @speaker = assign(:speaker, FactoryGirl.create(:speaker, 
       :first_name => "Bob",
@@ -12,21 +14,73 @@ RSpec.describe "speakers/show", type: :view do
     @speaker.talks = FactoryGirl.create_list(:talk, 2)
   end
 
-  it "has an ID that identifies the speaker" do 
-    render
-    expect(rendered).to have_selector "#speaker_#{@speaker.id}_profile"
+  context "when no user is logged in" do 
+    before(:each) do
+      def view.current_user; nil; end
+    end
+
+    it "has an ID that identifies the speaker" do 
+      render
+      expect(rendered).to have_selector "#speaker_#{@speaker.id}_profile"
+    end
+
+    it "renders attributes" do
+      render
+      expect(rendered).to match(/Bob Jones/)
+      expect(rendered).to match(/http\:\/\/www\.bobjones\.edu/)
+      expect(rendered).to match(/Bob Jones University/)
+    end
+
+    it "displays the speaker's talks" do 
+      render
+      expect(rendered).to have_content(@speaker.talks.first.title)
+      expect(rendered).to have_content(@speaker.talks.last.title)
+    end
+
+    it "doesn't display the edit link" do 
+      render
+      expect(rendered).not_to have_link 'Edit'
+    end
+
+    it "doesn't display the destroy link" do 
+      render
+      expect(rendered).not_to have_link 'Destroy'
+    end
   end
 
-  it "renders attributes" do
-    render
-    expect(rendered).to match(/Bob Jones/)
-    expect(rendered).to match(/http\:\/\/www\.bobjones\.edu/)
-    expect(rendered).to match(/Bob Jones University/)
-  end
+  context "when a user is logged in" do 
+    before(:each) do
+      @user = user
 
-  it "display's the speaker's talks" do 
-    render
-    expect(rendered).to have_content(@speaker.talks.first.title)
-    expect(rendered).to have_content(@speaker.talks.last.title)
+      def view.current_user; @user; end
+    end
+
+    it "has an ID that identifies the speaker" do 
+      render
+      expect(rendered).to have_selector "#speaker_#{@speaker.id}_profile"
+    end
+
+    it "renders attributes" do
+      render
+      expect(rendered).to match(/Bob Jones/)
+      expect(rendered).to match(/http\:\/\/www\.bobjones\.edu/)
+      expect(rendered).to match(/Bob Jones University/)
+    end
+
+    it "displays the speaker's talks" do 
+      render
+      expect(rendered).to have_content(@speaker.talks.first.title)
+      expect(rendered).to have_content(@speaker.talks.last.title)
+    end
+
+    it "displays the edit link" do 
+      render
+      expect(rendered).to have_link 'Edit'
+    end
+
+    it "displays the destroy link" do 
+      render
+      expect(rendered).to have_link 'Destroy'
+    end
   end
 end
