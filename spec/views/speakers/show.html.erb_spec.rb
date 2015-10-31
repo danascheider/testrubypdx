bb4f4ca1b0a373a1d741a45f4c012db6f4954ded
Dana Scheider <dana.scheider@gmail.com>
2015-10-31 13:26:36 -0700

    Display speaker's talks in their profile and give the whole profile a div ID to identify it as a speaker profile

--- spec/views/speakers/show.html.erb_spec.rb
+++ spec/views/speakers/show.html.erb_spec.rb
@@ -2,19 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "speakers/show", type: :view do
   before(:each) do
-    @speaker = assign(:speaker, Speaker.create!(
-      :first_name => "First Name",
-      :last_name => "Last Name",
-      :website => "Website",
-      :company => "Company"
+    @speaker = assign(:speaker, FactoryGirl.create(:speaker, 
+      :first_name => "Bob",
+      :last_name => "Jones",
+      :website => "http://www.bobjones.edu",
+      :company => "Bob Jones University"
     ))
+
+    @speaker.talks = FactoryGirl.create_list(:talk, 2)
   end
 
-  it "renders attributes in <p>" do
+  it "has an ID that identifies the speaker" do 
     render
-    expect(rendered).to match(/First Name/)
-    expect(rendered).to match(/Last Name/)
-    expect(rendered).to match(/Website/)
-    expect(rendered).to match(/Company/)
+    expect(rendered).to have_selector "#speaker_#{@speaker.id}_profile"
+  end
+
+  it "renders attributes" do
+    render
+    expect(rendered).to match(/Bob Jones/)
+    expect(rendered).to match(/http\:\/\/www\.bobjones\.edu/)
+    expect(rendered).to match(/Bob Jones University/)
+  end
+
+  it "display's the speaker's talks" do 
+    render
+    expect(rendered).to have_content(@speaker.talks.first.title)
+    expect(rendered).to have_content(@speaker.talks.last.title)
   end
 end
