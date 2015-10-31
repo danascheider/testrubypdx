96f08a40e93aca89e9e7a717d44037c391527808
Dana Scheider <dana.scheider@gmail.com>
2015-10-30 19:47:14 -0700

    Flesh out specs to cover case of whether or not user is logged in

--- spec/views/meetings/show.html.erb_spec.rb
+++ spec/views/meetings/show.html.erb_spec.rb
@@ -1,16 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "meetings/show", type: :view do
-  let(:user) { FactoryGirl.create(:user) }
+  let(:user) { FactoryGirl.create(:user, id: 1) }
+  let(:meeting) { FactoryGirl.create(:meeting) }
 
   before(:each) do
-    @meeting = assign(:meeting, FactoryGirl.create(:meeting))
+    def view.current_user; nil; end
+    @meeting = assign(:meeting, meeting)
   end
 
   it "renders the div" do
-    skip('Need to find out what current_user is being called on when it appears in the erb templates')
     render
+    expect(rendered).to have_css 'div.show-meeting'
+  end
 
-    expect(page).to have_css 'div.show-meeting'
+  it "doesn't show an edit link" do 
+    render
+    expect(rendered).not_to have_link 'Edit'
+  end
+
+  context "meeting with talks" do 
+    let(:talks) { FactoryGirl.create_list(:talks, 2, meeting_id: meeting.id) }
+
+    it "displays the talks" do
+      render
+      expect(rendered).to have_css ".talk", count: 2
+    end
+  end
+
+  context "logged-in user" do 
+    before(:each) do 
+      @user = user
+      def view.current_user; @user; end
+    end
+
+    it "renders the div" do 
+      render
+      expect(rendered).to have_css 'div.show-meeting'
+    end
+
+    it "shows an edit link" do 
+      render
+      expect(rendered).to have_link 'Edit' 
+    end
   end
 end
