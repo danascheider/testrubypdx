1dd233822f2713813c7fd0016068a378a7ec54a0
Dana Scheider <dana.scheider@gmail.com>
2015-11-01 08:52:21 -0800

    Tweak Meeting factories a little more because of weird recurring validation error

--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :meeting do
-    sequence :date do 
-      "#{rand(2000..2100)}-#{rand(13)}-#{rand(32)} #{rand(24)}:#{rand(61)}:#{rand(61)}"
+    sequence :date do |n|
+      Date.tomorrow - n.weeks
     end
 
     factory :past_meeting do 
-      sequence :date do 
-        "#{rand(2015)}-#{rand(13)}-#{rand(32)} 18:00:00"
+      sequence :date do |n|
+        Date.today - n.years
       end
     end
 
     factory :upcoming_meeting do 
       sequence :date do 
-        "#{rand(300) + 2017}-#{rand(13)}-#{rand(32)} 18:00:00"
+        "2020-#{rand(13)}-#{rand(32)} #{rand(24)}:#{rand(61)}:00"
       end
     end
   end
