0baf437b1b4067f82cd7a8d071ae845b12e8ffc8
Dana Scheider <dana.scheider@gmail.com>
2015-11-01 08:30:42 -0800

    Make dates into sequences so meetings are differentiated

--- spec/factories/meetings.rb
+++ spec/factories/meetings.rb
@@ -1,13 +1,19 @@
 FactoryGirl.define do
   factory :meeting do
-    date "2015-10-22 18:12:44"
+    sequence :date do 
+      "#{rand(2000..2100)}-#{rand(13)}-#{rand(32)} #{rand(24)}:#{rand(61)}:#{rand(61)}"
+    end
 
     factory :past_meeting do 
-      date "2015-10-22 18:00:00"
+      sequence :date do 
+        "#{rand(2015)}-#{rand(13)}-#{rand(32)} 18:00:00"
+      end
     end
 
     factory :upcoming_meeting do 
-      date "2100-10-22 18:00:00"
+      sequence :date do 
+        "#{rand(2017..2200)}-#{rand(13)}-#{rand(32)} 18:00:00"
+      end
     end
   end
 end
