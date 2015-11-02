danascheider/testrubypdx

commit 458cd3ef6d490df6f02c1aa103a21a770bad6930

Change wording of steps for the /speakers/:id/talks routes

features/routing/speaker_talk_routes.feature
@@ -7,13 +7,13 @@ Feature: Speaker talk routes
     And I am logged in
 
   Scenario: Visit route for a given speaker's talks
-    When I visit the page for that speaker's talks
+    When I visit the page for the first speaker's talks
     Then I should see all that speaker's talks
     And I should not see the other talks
 
   Scenario: Create a talk for a particular speaker
-    When I visit the page to create a talk for that speaker
-    And I fill in the '#new_talk' form with the following attributes:
+    When I visit the page to create a talk for the first speaker
+    And I submit the '#new_talk' form with the following attributes:
       | title            |
       | How to Use RSpec |
     Then the speaker should have a talk called 'How to Use RSpec'

features/step_definitions/routing_steps/routing_steps.rb
@@ -97,7 +97,7 @@ Given /^the last speaker has (\d+) talks$/ do |count|
   FactoryGirl.create_list(:talk, count.to_i, speaker_id: Speaker.last.id)
 end
 
-When /^I fill in the '\#new_talk' form with the following attributes:$/ do |table|
+When /^I submit the '\#new_talk' form with the following attributes:$/ do |table|
   attributes = table.hashes.first
 
   within '#new_talk' do 
@@ -109,11 +109,12 @@ When /^I fill in the '\#new_talk' form with the following attributes:$/ do |tabl
   end
 end
 
-When /^I visit the page to create a talk for that speaker$/ do 
+When /^I visit the page to create a talk for the first speaker$/ do 
   visit "/speakers/#{@speaker.id}/talks/new"
 end
 
-When /^I visit the page for that speaker's talks$/ do 
+When /^I visit the page for the first speaker's talks$/ do 
+  @speaker = Speaker.first
   visit "/speakers/#{@speaker.id}/talks"
 end
 
@@ -135,5 +136,42 @@ Then /^I should not see the other talks$/ do
 end
 
 Then /^the speaker should have a talk called '([^']*)'$/ do |title|
-  expect(Talk.where(title: title, speaker_id: @speaker.id)).to be_truthy
+  expect(Talk.where(title: title, speaker_id: @speaker.id).count).to eql 1
+end
+
+####### Meetings-Talks #######
+
+Given /^the first meeting has (\d+) talks$/ do |count|
+  @meeting = Meeting.first
+  FactoryGirl.create_list(:talk, count.to_i, meeting_id: @meeting.id)
+end
+
+Given /^the last meeting has (\d+) talks$/ do |count|
+  FactoryGirl.create_list(:talk, count.to_i, meeting_id: Meeting.last.id)
+end
+
+When /^I visit the page with the talks for the meeting$/ do 
+  visit "/meetings/#{@meeting.id}/talks"
+end
+
+When /^I visit the page to create a talk for the first meeting$/ do 
+  @meeting = Meeting.first
+  visit "/meetings/#{@meeting.id}/talks/new"
+end
+
+Then /^I should see the talks for that meeting$/ do 
+  @meeting.talks.each do |talk|
+    expect(page).to have_content(talk.title)
+  end
+end
+
+Then /^I should not see the talks for the other meeting$/ do 
+  talks = Talk.where("meeting_id != ?", @meeting.id)
+  talks.each do |talk|
+    expect(page).not_to have_content(talk.title)
+  end
+end
+
+Then /^the meeting should have a talk called '([^']*)'$/ do |title|
+  expect(Talk.where(title: title, meeting_id: @meeting.id).count).to eql 1
 end

commit 97e345e134a991bb66b7f48bb277b97e1b0678d8

Finish defining speaker/talk steps but not meeting/talk steps, in an effort to narrow down what about commit 458cd3e broke the build

features/routing/speaker_talk_routes.feature
@@ -7,13 +7,13 @@ Feature: Speaker talk routes
     And I am logged in
 
   Scenario: Visit route for a given speaker's talks
-    When I visit the page for that speaker's talks
+    When I visit the page for the first speaker's talks
     Then I should see all that speaker's talks
     And I should not see the other talks
 
   Scenario: Create a talk for a particular speaker
-    When I visit the page to create a talk for that speaker
-    And I fill in the '#new_talk' form with the following attributes:
+    When I visit the page to create a talk for the first speaker
+    And I submit the '#new_talk' form with the following attributes:
       | title            |
       | How to Use RSpec |
     Then the speaker should have a talk called 'How to Use RSpec'

features/step_definitions/routing_steps/routing_steps.rb
@@ -97,7 +97,7 @@ Given /^the last speaker has (\d+) talks$/ do |count|
   FactoryGirl.create_list(:talk, count.to_i, speaker_id: Speaker.last.id)
 end
 
-When /^I fill in the '\#new_talk' form with the following attributes:$/ do |table|
+When /^I submit the '\#new_talk' form with the following attributes:$/ do |table|
   attributes = table.hashes.first
 
   within '#new_talk' do 
@@ -109,11 +109,13 @@ When /^I fill in the '\#new_talk' form with the following attributes:$/ do |tabl
   end
 end
 
-When /^I visit the page to create a talk for that speaker$/ do 
+When /^I visit the page to create a talk for the first speaker$/ do 
+  @speaker = Speaker.first
   visit "/speakers/#{@speaker.id}/talks/new"
 end
 
-When /^I visit the page for that speaker's talks$/ do 
+When /^I visit the page for the first speaker's talks$/ do 
+  @speaker = Speaker.first
   visit "/speakers/#{@speaker.id}/talks"
 end
 