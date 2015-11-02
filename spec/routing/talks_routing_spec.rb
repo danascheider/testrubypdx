5f1ab67557d17caddcd4ec3b4e147e2247c3f17a
Dana Scheider <dana.scheider@gmail.com>
2015-11-01 22:27:22 -0800

    Add tests for nested routes

--- spec/routing/talks_routing_spec.rb
+++ spec/routing/talks_routing_spec.rb
@@ -3,37 +3,66 @@ require "rails_helper"
 RSpec.describe TalksController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/talks").to route_to("talks#index")
+    context "normal routes" do 
+      it "routes to #index" do
+        expect(:get => "/talks").to route_to("talks#index")
+      end
+
+      it "routes to #new" do
+        expect(:get => "/talks/new").to route_to("talks#new")
+      end
+
+      it "routes to #show" do
+        expect(:get => "/talks/1").to route_to("talks#show", :id => "1")
+      end
+
+      it "routes to #edit" do
+        expect(:get => "/talks/1/edit").to route_to("talks#edit", :id => "1")
+      end
+
+      it "routes to #create" do
+        expect(:post => "/talks").to route_to("talks#create")
+      end
+
+      it "routes to #update via PUT" do
+        expect(:put => "/talks/1").to route_to("talks#update", :id => "1")
+      end
+
+      it "routes to #update via PATCH" do
+        expect(:patch => "/talks/1").to route_to("talks#update", :id => "1")
+      end
+
+      it "routes to #destroy" do
+        expect(:delete => "/talks/1").to route_to("talks#destroy", :id => "1")
+      end
     end
 
-    it "routes to #new" do
-      expect(:get => "/talks/new").to route_to("talks#new")
+    context "nested in meetings" do 
+      it "routes to #index" do 
+        expect(:get => "/meetings/1/talks").to route_to("talks#index", :meeting_id => "1")
+      end
+
+      it "routes to #new" do 
+        expect(:get => "/meetings/1/talks/new").to route_to("talks#new", :meeting_id => "1")
+      end
+
+      it "routes to #create" do 
+        expect(:post => "/meetings/1/talks").to route_to("talks#create", :meeting_id => "1")
+      end
     end
 
-    it "routes to #show" do
-      expect(:get => "/talks/1").to route_to("talks#show", :id => "1")
-    end
+    context "nested in speakers" do 
+      it "routes to #index" do 
+        expect(:get => "/speakers/1/talks").to route_to("talks#index", :speaker_id => "1")
+      end
 
-    it "routes to #edit" do
-      expect(:get => "/talks/1/edit").to route_to("talks#edit", :id => "1")
-    end
+      it "routes to #new" do 
+        expect(:get => "/speakers/1/talks/new").to route_to("talks#new", :speaker_id => "1")
+      end
 
-    it "routes to #create" do
-      expect(:post => "/talks").to route_to("talks#create")
+      it "routes to #create" do 
+        expect(:post => "/speakers/1/talks").to route_to("talks#create", :speaker_id => "1")
+      end
     end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/talks/1").to route_to("talks#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/talks/1").to route_to("talks#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/talks/1").to route_to("talks#destroy", :id => "1")
-    end
-
   end
 end
