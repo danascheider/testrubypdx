083f69cda356aaaac0019dbd30da1bff4a1f168e
Dana Scheider <dana.scheider@gmail.com>
2015-12-10 11:15:04 -0800

    Basic seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+speakers = Speaker.create([{first_name: 'Freddie', last_name: 'Mercury'}, {first_name: 'Brian', last_name: 'May'}, {first_name: 'John', last_name: 'Deacon'}])
+talks    = Talk.create([{title: 'Another One Bites the Dust', speaker: Speaker.first}, {title: 'I Want to Break Free', speaker: Speaker.find_by(first_name: 'Brian')}, {title: "You're My Best Friend", speaker: Speaker.last}])
+venue    = Venue.create(name: 'Renew Financial', address_1: '400 SW 6th Ave.')
+meetings = Meeting.create([{date: Date.yesterday, venue: Venue.first}, {date: Date.tomorrow, venue: Venue.first}])
